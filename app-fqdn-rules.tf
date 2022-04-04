d39624e3aba4cf6738c702e6589e8829cfde76e8
Drizz1211DO <102941866+Drizz1211DO@users.noreply.github.com>
2022-04-04 22:36:08 +0200

    Update app-fqdn-rules.tf

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -2,10 +2,12 @@
 locals {
   egress_rules = {
     tcp = {
-      "*.aviatrix.com" = "443"
-      "aviatrix.com"   = "80"
-      "*.ubuntu.com"   = "80"
-      "haribo.de"      = "443"
+      "*.aviatrix.com"  = "443"
+      "aviatrix.com"    = "80"
+      "*.ubuntu.com"    = "80"
+      "haribo.de"       = "443"
+      "abc.com"         = "8080"
+      "computerbase.de" = "443"
     }
     udp = {
       "dns.google.com" = "53"
